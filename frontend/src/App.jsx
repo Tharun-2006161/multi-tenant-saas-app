455fa5c954d5926379cb91a83e39fb609d8aae06
Tharun-2006161 <23A91A05F5@aec.edu.in>
2025-12-26 23:02:41 +0530

    Fix: Hard-coding filename case for Vercel

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-
 import Login from "./components/Login";
 import Register from "./components/Register";
 import Dashboard from "./components/Dashboard";
 import ProtectedRoute from "./components/ProtectedRoute";
-
 const App = () => {
   return (
     <Router>
